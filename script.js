cc89bdb175385983e2ebc3991986291d14c96e75
corel67 <corentin.67@gmail.com>
2023-11-11 02:39:11 +0100

    Finalisation du menu de navigation

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const html = document.getElementsByTagName("html")[0]
+const navList = document.getElementById("navList")
+
 document.getElementById("navToggle").addEventListener("click", function () {
-    document.getElementById("navList").classList.toggle("open")
-    document.getElementsByTagName("html")[0].classList.toggle("disable-scroll")
+    navList.classList.toggle("open")
+    html.classList.toggle("disable-scroll")
+})
+
+navList.querySelectorAll("a").forEach(element => {
+    element.addEventListener("click", function () {
+        navList.classList.remove("open")
+        html.classList.remove("disable-scroll")
+    })
 })
